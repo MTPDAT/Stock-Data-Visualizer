c6c8c797e7a6d7603458946f6edb3253e8c7f3d1
Mako Chirisa <notmako@MacBook-Air-27.local>
2026-04-02 15:07:44 -0500

    feat: setup functions that get date from user, and validates start and end.

---
+++ main.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+"""
+get_valid_date() - Gets date in YYYY-MM-DD format.
+"""
+def get_valid_date(prompt):
+    while True:
+        date_str = input(prompt)
+        try:
+           return datetime.strptime(date_str, "%Y-%m-%d") 
+        except ValueError: 
+            print("Error: Please use YYYY-MM-DD.")
+
+"""
+get_date_range() - Uses get_valid_date() and checks that the end date should not be before the begin date
+"""
+def get_date_range():
+    start_date = get_valid_date("Enter the beginning date (YYYY-MM-DD): ")
+
+    while True:
+        end_date = get_valid_date("Enter the end date (YYYY-MM-DD): ")
+
+        #Logic: End date should not be before the begin date
+        if end_date >= start_date:
+            return start_date, end_date
+        else:
+            print(f"Error: End date cannot be before start date.")
